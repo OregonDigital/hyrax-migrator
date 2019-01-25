ba827d0b3c1a3a508306b6b219379f8cccf545e7
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2019-01-25 10:41:46 -0800

    updated argument variables

--- app/services/hyrax/migrator/file_upload_service.rb
+++ app/services/hyrax/migrator/file_upload_service.rb
@@ -1,31 +1,26 @@
 module Hyrax
   module Migrator
     class FileUploadService
-      attr_reader :aws_s3_app_key, :aws_s3_app_secret,
-                  :aws_s3_bucket, :aws_s3_region,
-                  :file_system_path
+      attr_reader :work, :file_system_path,
+                  :aws_s3_app_key, :aws_s3_app_secret,
+                  :aws_s3_bucket, :aws_s3_region
 
-      # @param file_set_content [File]
+      # @param work [Work]
       # @param file_system_path [String] 
       # @param aws_s3_app_key [String]
       # @param aws_s3_app_secret [String]
       # @param latest_version_only [String]
-      def initialize(file_set_content,
-        file_system_path: Hyrax::Migrator.config.file_system_path,
-        aws_s3_app_key: Hyrax::Migrator.config.aws_s3_app_key,
-        aws_s3_app_secret: Hyrax::Migrator.config.aws_s3_app_secret,
-        aws_s3_bucket: Hyrax::Migrator.config.aws_s3_bucket,
-        aws_s3_region: Hyrax::Migrator.config.aws_s3_region)
-
-        @file_system_path = file_system_path
-        @aws_s3_app_key = aws_s3_app_key
-        @aws_s3_app_secret = aws_s3_app_secret
-        @aws_s3_bucket = aws_s3_bucket
-        @aws_s3_region = aws_s3_region
+      def initialize(work, config)
+        @work = work
+        @file_system_path = config.file_system_path
+        @aws_s3_app_key = config.aws_s3_app_key
+        @aws_s3_app_secret = config.aws_s3_app_secret
+        @aws_s3_bucket = config.aws_s3_bucket
+        @aws_s3_region = config.aws_s3_region
       end
 
       def upload_file_content
-        #TODO: upload file content to proper storage service
+        #TODO: upload file content to proper storage place using active storage
         false
       end
     end
--- spec/hyrax/migrator/file_upload_service_spec.rb
+++ spec/hyrax/migrator/file_upload_service_spec.rb
@@ -1,5 +1,17 @@
 RSpec.describe Hyrax::Migrator::FileUploadService do
-  let(:service) { described_class.new("test file") } 
+
+
+  let(:config) { Hyrax::Migrator::Configuration.new }
+  let(:service) { described_class.new("test file", config) } 
+
+  before do
+    config.file_system_path = "/data/tmp"
+    config.aws_s3_app_key = "test"
+    config.aws_s3_app_secret = "test"
+    config.aws_s3_bucket = "test"
+    config.aws_s3_region = "test"
+  end
+
   describe 'upload_file_content' do
     # TODO: retrieve proper fixture to test and implement specs for uploading files based on environment
     it 'uploads content to proper destination' do
