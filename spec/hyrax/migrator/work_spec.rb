27f82a2be7528a0ca36b77e0edcdb9daa7db646b
Josh Gum <revgum@gmail.com>
2019-01-25 15:50:50 -0800

    test that work model has constants

--- spec/hyrax/migrator/work_spec.rb
+++ spec/hyrax/migrator/work_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Hyrax::Migrator::Work do
   let(:model) { create(:work) }
 
   it { expect(model.env).to be_a Hash }
+  it { expect(described_class::SUCCESS).to eq('success') }
+  it { expect(described_class::FAIL).to eq('fail') }
 end
