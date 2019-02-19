c8610f1acbbce5b8874b61346510b1d22d8126fe
Josh Gum <revgum@gmail.com>
2019-02-19 10:53:00 -0800

    fixes bug in persist work actor calling user (#74)

--- app/actors/hyrax/migrator/actors/persist_work_actor.rb
+++ app/actors/hyrax/migrator/actors/persist_work_actor.rb
@@ -37,7 +37,7 @@ module Hyrax::Migrator::Actors
 
     #:nocov:
     def service
-      @service ||= Hyrax::Migrator::Services::PersistWorkService.new(@work, @user)
+      @service ||= Hyrax::Migrator::Services::PersistWorkService.new(@work, user)
     end
     #:nocov:
 
