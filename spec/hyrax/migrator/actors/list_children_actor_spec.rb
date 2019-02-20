ae9f1546cc0af677cf01916e5eec7df0926d7b6c
lsat12357 <lsato@uoregon.edu>
2019-02-20 08:03:33 -0800

    list_children_actor

---
+++ spec/hyrax/migrator/actors/list_children_actor_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rdf'
+
+RSpec.describe Hyrax::Migrator::Actors::ListChildrenActor do
+  let(:actor) { described_class.new }
+  let(:terminal) { Hyrax::Migrator::Actors::TerminalActor.new }
+  let(:work) { create(:work, pid: pid) }
+  let(:pid) { 'abcde1234' }
+  let(:config) { Hyrax::Migrator::Configuration.new }
+  let(:result_hash) { instance_double(Hash) }
+  let(:lcs) { instance_double('Hyrax::Migrator::Services::ListChildrenService') }
+
+  describe '#create' do
+    context 'when the hash is successfully created' do
+      before do
+        allow(actor).to receive(:config).and_return(config)
+        allow(Hyrax::Migrator::Services::ListChildrenService).to receive(:new).and_return(lcs)
+        allow(lcs).to receive(:list_children).and_return(result_hash)
+        actor.next_actor = terminal
+      end
+
+      it 'updates the work' do
+        actor.create(work)
+        expect(work.aasm_state).to eq('list_children_succeeded')
+      end
+      it 'calls the next actor' do
+        expect(terminal).to receive(:create)
+        actor.create(work)
+      end
+    end
+
+    context 'when list_children_service fails' do
+      let(:error) { StandardError }
+
+      before do
+        allow(actor).to receive(:config).and_return(config)
+        allow(Hyrax::Migrator::Services::ListChildrenService).to receive(:new).and_return(lcs)
+        allow(lcs).to receive(:list_children).and_raise(error)
+        actor.next_actor = terminal
+      end
+
+      it 'updates the work' do
+        actor.create(work)
+        expect(work.aasm_state).to eq('list_children_failed')
+      end
+      it 'does not call the next actor' do
+        expect(terminal).not_to receive(:create)
+        actor.create(work)
+      end
+    end
+  end
+end
