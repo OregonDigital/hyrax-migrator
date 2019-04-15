34d55c7e7c8d61901ee2e58824d8353a91e6ff16
lsat12357 <lsato@uoregon.edu>
2019-04-15 07:49:43 -0700

    List children service (#79)
    * add service
    
    * return empty

---
+++ app/services/hyrax/migrator/services/list_children_service.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal:true
+
+require 'rdf'
+require 'rdf/ntriples'
+
+module Hyrax::Migrator::Services
+  # Called by the ListChildrenActor
+  class ListChildrenService
+    def initialize(work, migrator_config)
+      @work = work
+      @config = migrator_config
+    end
+
+    def list_children
+      children = {}
+      return children if @work.env[:attributes][:contents].blank?
+
+      @work.env[:attributes][:contents].each_with_index do |item, index|
+        child_id = item.gsub('http://oregondigital.org/resource/oregondigital:', '')
+        children[index.to_s] = { 'id' => child_id }
+      end
+      children
+    end
+  end
+end
---
+++ spec/hyrax/migrator/services/list_children_service_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+RSpec.describe Hyrax::Migrator::Services::ListChildrenService do
+  let(:pid) { '3t945r08v' }
+  let(:file_path) { File.join(Rails.root, '..', 'fixtures', pid) }
+  let(:config) { Hyrax::Migrator::Configuration.new }
+  let(:work) { create(:work, pid: pid, file_path: file_path) }
+  let(:service) { described_class.new(work, config) }
+  let(:result_hash) do
+    h = {}
+    h['0'] = { 'id' => 'abcde1234' }
+    h['1'] = { 'id' => 'abcde1235' }
+    h
+  end
+  let(:resource1) { 'http://oregondigital.org/resource/oregondigital:abcde1234' }
+  let(:resource2) { 'http://oregondigital.org/resource/oregondigital:abcde1235' }
+
+  before do
+    work.env[:attributes] = {}
+    work.env[:attributes][:contents] = [resource1, resource2]
+  end
+
+  describe 'list_children' do
+    context 'when there are children' do
+      it 'puts them in a hash, in order' do
+        expect(service.list_children).to eq(result_hash)
+      end
+    end
+
+    context 'when there are no children' do
+      before do
+        work.env[:attributes] = {}
+      end
+
+      it 'returns an empty hash' do
+        expect(service.list_children).to be_empty
+      end
+    end
+  end
+end
