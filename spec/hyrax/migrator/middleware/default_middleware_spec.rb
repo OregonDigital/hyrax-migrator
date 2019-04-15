73588b1349519e2bed1bb54736978c4663e1d2c2
Josh Gum <revgum@gmail.com>
2019-04-15 10:36:43 -0700

    fixes coverage for default middleware

--- spec/hyrax/migrator/middleware/default_middleware_spec.rb
+++ spec/hyrax/migrator/middleware/default_middleware_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Hyrax::Migrator::Middleware::DefaultMiddleware do
   it { is_expected.to respond_to(:actor_stack) }
   it { expect(middleware.actor_stack).to eq nil }
   it { is_expected.to respond_to(:start) }
-  it { expect(Hyrax::Migrator::Middleware.default.actor_stack.class).to eq Hyrax::Migrator::Actors::BagValidatorActor }
+  it { expect(Hyrax::Migrator::Middleware.default).to be_a described_class }
 
   context 'with an array of actors' do
     let(:actors) { [TestActor, TestActor, TerminalTestActor] }
