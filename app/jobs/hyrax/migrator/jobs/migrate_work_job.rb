dbcd4f773a2a9b97b4ba55c43bf671e685a62ffe
Josh Gum <revgum@gmail.com>
2019-04-15 12:34:18 -0700

    migrate work service and related job

--- app/jobs/hyrax/migrator/jobs/migrate_work_job.rb
+++ app/jobs/hyrax/migrator/jobs/migrate_work_job.rb
@@ -5,30 +5,9 @@ module Hyrax
     ##
     # The job responsible for initiating migrating a work
     class MigrateWorkJob < ApplicationJob
-      def initialize(args)
-        @pid = args[:pid]
-        @file_path = args[:file_path]
-      end
-
-      def run
-        middleware.start(work)
-      end
-
-      def work
-        @work ||= find_or_create_work(@pid, @file_path)
-      end
-
-      def middleware
-        @middleware ||= Hyrax::Migrator::Middleware.default
-      end
-
-      private
-
-      def find_or_create_work(pid, file_path)
-        work = Hyrax::Migrator::Work.where(pid: pid).first
-        return work if work
-
-        Hyrax::Migrator::Work.create(pid: pid, file_path: file_path)
+      def perform(args)
+        service = ::Services::MigrateWorkService.new(args)
+        service.run
       end
     end
   end
