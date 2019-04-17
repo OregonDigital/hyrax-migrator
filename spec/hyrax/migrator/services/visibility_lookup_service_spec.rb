235d81984c960b2ad0e0842c4cb0390d748b18ca
lsat12357 <lsato@uoregon.edu>
2019-04-17 08:18:55 -0700

    Visibility service (#98)
    * visibility service
    
    * test lookup
    
    * test read_groups

---
+++ spec/hyrax/migrator/services/visibility_lookup_service_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal:true
+
+require 'nokogiri'
+RSpec.describe Hyrax::Migrator::Services::VisibilityLookupService do
+  let(:config) { Hyrax::Migrator::Configuration.new }
+  let(:service) { described_class.new(work, config) }
+  let(:file_path) { File.join(Rails.root, '..', 'fixtures', pid) }
+  let(:work) { create(:work, pid: pid, file_path: file_path) }
+  let(:pid) { '3t945r08v' }
+
+  describe '#visibility_lookup' do
+    context 'with a missing xml file in the bag manifest' do
+      before do
+        stub_const('Hyrax::Migrator::Services::VisibilityLookupService::XML_FILE', 'bogus_file.xml')
+      end
+
+      it { expect { service.lookup_visibility }.to raise_error(StandardError, "could not find an xml file ending with '#{described_class::XML_FILE}' in #{work.file_path}/data") }
+    end
+
+    context 'with a missing xml file in the directory' do
+      before do
+        allow(service).to receive(:xml_file).and_return('mocking_a_bogus_file_in_manifest.xml')
+      end
+
+      it { expect { service.lookup_visibility }.to raise_error StandardError, "could not find xml file mocking_a_bogus_file_in_manifest.xml in #{work.file_path}/data" }
+    end
+
+    context 'with an invalid data directory' do
+      let(:file_path) { 'bogus/path' }
+
+      it { expect { service.lookup_visibility }.to raise_error StandardError, "data directory #{work.file_path}/data not found" }
+    end
+
+    context 'with an invalid xml node xpath' do
+      before do
+        stub_const('Hyrax::Migrator::Services::VisibilityLookupService::XML_NODE', '//bob/ross/GOAT')
+      end
+
+      it { expect { service.lookup_visibility }.to raise_error(StandardError, "could not find #{described_class::XML_NODE} in xml file") }
+    end
+  end
+
+  describe '#read_groups' do
+    context 'when the file is valid' do
+      let(:xml) { '<rightsMetadata><access type=\'read\'><group>public</group></access></rightsMetadata>' }
+      let(:nodes) { Nokogiri::XML(xml).search('group') }
+      let(:doc) { instance_double(Nokogiri::XML::Document) }
+
+      before do
+        allow(service).to receive(:doc).and_return(doc)
+        allow(doc).to receive(:search).and_return(nodes)
+      end
+
+      it 'extracts the groups' do
+        expect(service.send(:read_groups)).to eq(%w[public])
+      end
+    end
+  end
+
+  describe '#lookup' do
+    context 'when the original group is public' do
+      let(:groups) { %w[public admin archivist] }
+
+      it 'returns open' do
+        expect(service.send(:lookup, groups)).to eq(visibility: 'open')
+      end
+    end
+
+    context 'when the original group is an institution' do
+      let(:groups) { %w[admin archivist University-of-Oregon] }
+
+      it 'returns authenticated' do
+        expect(service.send(:lookup, groups)).to eq(visibility: 'authenticated')
+      end
+    end
+
+    context 'when the original group is none of the above' do
+      let(:groups) { %w[admin archivist] }
+
+      it 'returns restricted' do
+        expect(service.send(:lookup, groups)).to eq(visibility: 'restricted')
+      end
+    end
+  end
+end
