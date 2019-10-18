8fb03695998d7a81f123e1d6ba87d021c40701a0
lsat12357 <lsato@uoregon.edu>
2019-10-18 09:44:48 -0700

    add specs

---
+++ spec/hyrax/migrator/middleware_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal:true
+
+RSpec.describe Hyrax::Migrator::Middleware do
+  let(:middleware) { described_class }
+
+  describe 'default' do
+    let(:config) { Hyrax::Migrator::Middleware::Configuration.new }
+
+    it 'returns the default middleware' do
+      expect(middleware.default.actor_stack).to respond_to :bag_validator_initial
+    end
+  end
+
+  describe 'custom' do
+    let(:actor_stack) { [Hyrax::Migrator::Actors::CrosswalkMetadataActor] }
+    let(:config) do
+      c = Hyrax::Migrator::Middleware::Configuration.new
+      c.actor_stack = actor_stack
+      c
+    end
+
+    it 'returns middleware with a custom stack' do
+      expect(middleware.custom(config).actor_stack).to respond_to :crosswalk_metadata_initial
+    end
+  end
+end
