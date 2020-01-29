91a8a24737d51a040b29a66f85ad94d1925dee50
lsat12357 <lsato@uoregon.edu>
2020-01-29 14:45:36 -0800

    use graph in list children

--- app/services/hyrax/migrator/services/list_children_service.rb
+++ app/services/hyrax/migrator/services/list_children_service.rb
@@ -6,20 +6,31 @@ require 'rdf/ntriples'
 module Hyrax::Migrator::Services
   # Called by the ListChildrenActor
   class ListChildrenService
+    CONTENTS_PREDICATE = 'http://opaquenamespace.org/ns/contents'
     def initialize(work, migrator_config)
       @work = work
+      @data_dir = File.join(work.working_directory, 'data')
       @config = migrator_config
+      @graph = create_graph
     end
 
     def list_children
       children = {}
-      return children if @work.env[:attributes][:contents].blank?
+      return children if contents.blank?
 
-      @work.env[:attributes][:contents].each_with_index do |item, index|
+      contents.each_with_index do |item, index|
         child_id = item.gsub('http://oregondigital.org/resource/oregondigital:', '')
         children[index.to_s] = { 'id' => child_id }
       end
       children
+    end
+
+    def contents
+      @graph.statements.select { |s| s.predicate.to_s.casecmp(CONTENTS_PREDICATE).zero? }.map { |r| r.object.to_s }
+    end
+
+    def create_graph
+      Hyrax::Migrator::Services::CreateGraphService.call(@data_dir)
     end
   end
 end
