ab660e18c9b8af197d9b26f3e667f4be84bbfd99
lsat12357 <lsato@uoregon.edu>
2020-02-03 11:29:16 -0800

    use graph in list children (#178)
    * use graph in list children
    
    * add fixture for spec

--- spec/hyrax/migrator/services/list_children_service_spec.rb
+++ spec/hyrax/migrator/services/list_children_service_spec.rb
@@ -1,24 +1,22 @@
 # frozen_string_literal: true
 
+require 'rdf'
+require 'rdf/ntriples'
+
 RSpec.describe Hyrax::Migrator::Services::ListChildrenService do
-  let(:pid) { '3t945r08v' }
+  let(:pid) { 'df72jn67c' }
   let(:file_path) { File.join(Rails.root, '..', 'fixtures', pid) }
   let(:config) { Hyrax::Migrator::Configuration.new }
   let(:work) { create(:work, pid: pid, file_path: file_path) }
   let(:service) { described_class.new(work, config) }
   let(:result_hash) do
     h = {}
-    h['0'] = { 'id' => 'abcde1234' }
-    h['1'] = { 'id' => 'abcde1235' }
+    h['0'] = { 'id' => 'df72jn000' }
+    h['1'] = { 'id' => 'df72jn018' }
     h
   end
-  let(:resource1) { 'http://oregondigital.org/resource/oregondigital:abcde1234' }
-  let(:resource2) { 'http://oregondigital.org/resource/oregondigital:abcde1235' }
-
-  before do
-    work.env[:attributes] = {}
-    work.env[:attributes][:contents] = [resource1, resource2]
-  end
+  let(:resource1) { 'http://oregondigital.org/resource/oregondigital:df72jn000' }
+  let(:resource2) { 'http://oregondigital.org/resource/oregondigital:df72jn018' }
 
   describe 'list_children' do
     context 'when there are children' do
@@ -28,8 +26,10 @@ RSpec.describe Hyrax::Migrator::Services::ListChildrenService do
     end
 
     context 'when there are no children' do
+      let(:graph) { RDF::Graph.load('spec/fixtures/3t945r08v/data/3t945r08v_descMetadata.nt') }
+
       before do
-        work.env[:attributes] = {}
+        allow(Hyrax::Migrator::Services::CreateGraphService).to receive(:call).and_return(graph)
       end
 
       it 'returns an empty hash' do
