52c77ebef90bd19f225d58169e6eb1da445deb89
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2020-03-19 17:08:46 -0700

    add specs for verify_content

--- spec/hyrax/migrator/services/verify_metadata_service_spec.rb
+++ spec/hyrax/migrator/services/verify_metadata_service_spec.rb
@@ -1,25 +1,30 @@
 # frozen_string_literal: true
 
 RSpec.describe Hyrax::Migrator::Services::VerifyMetadataService do
+  let(:migrator_work) { double }
+  let(:hyrax_work) { double }
+  let(:config) { Hyrax::Migrator::Configuration.new }
+  let(:service) { described_class.new(migrator_work, config, 'spec/fixtures/data') }
+  let(:pid) { 'df70jh899' }
+  let(:set) { double }
+  let(:sets) { [set] }
+  let(:file_set) { instance_double('FileSet', id: 'bn999672v', uri: 'http://127.0.0.1/rest/fake/bn/99/96/72/bn999672v') }
+  let(:content_path) { 'spec/fixtures/data/world.png' }
+  let(:original_file) { instance_double('hyrax_original_file', content: File.open(content_path).read) }
+
+  before do
+    allow(Hyrax::Migrator::HyraxCore::Asset).to receive(:find).and_return(hyrax_work)
+    allow(hyrax_work).to receive(:as_json).and_return(json)
+    allow(hyrax_work).to receive(:member_of_collections).and_return(sets)
+    allow(hyrax_work).to receive(:file_sets).and_return([file_set])
+    allow(file_set).to receive(:original_file).and_return(original_file)
+    allow(migrator_work).to receive(:pid).and_return(pid)
+    config.fields_map = 'spec/fixtures/fields_map.yml'
+    allow(set).to receive(:id).and_return('joel-palmer')
+    allow(hyrax_work).to receive(:admin_set_id).and_return('uo-scua')
+  end
+
   describe 'verify_metadata' do
-    let(:migrator_work) { double }
-    let(:hyrax_work) { double }
-    let(:config) { Hyrax::Migrator::Configuration.new }
-    let(:service) { described_class.new(migrator_work, config, 'spec/fixtures/data') }
-    let(:pid) { 'df70jh899' }
-    let(:set) { double }
-    let(:sets) { [set] }
-
-    before do
-      allow(Hyrax::Migrator::HyraxCore::Asset).to receive(:find).and_return(hyrax_work)
-      allow(hyrax_work).to receive(:as_json).and_return(json)
-      allow(hyrax_work).to receive(:member_of_collections).and_return(sets)
-      allow(migrator_work).to receive(:pid).and_return(pid)
-      config.fields_map = 'spec/fixtures/fields_map.yml'
-      allow(set).to receive(:id).and_return('joel-palmer')
-      allow(hyrax_work).to receive(:admin_set_id).and_return('uo-scua')
-    end
-
     context 'when all of the metadata is present' do
       let(:json) do
         hash = {}
@@ -46,6 +51,24 @@ RSpec.describe Hyrax::Migrator::Services::VerifyMetadataService do
 
       it 'returns the error' do
         expect(service.verify_metadata).to eq(["Unable to verify identifier in #{pid}."])
+      end
+    end
+  end
+
+  describe 'verify_content' do
+    let(:json) { {} }
+
+    context 'when all checksums values match migrated content' do
+      it 'returns no errors' do
+        expect(service.verify_content).to eq([])
+      end
+    end
+
+    context 'when checksums do not match with migrated content (invalid)' do
+      let(:original_file) { instance_double('hyrax_original_file', content: 'invalid') }
+
+      it 'returns errors' do
+        expect(service.verify_content).to include(match(/Content does not match precomputed/))
       end
     end
   end
