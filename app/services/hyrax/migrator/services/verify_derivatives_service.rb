1817589123409b88059133f75ec2eeafd05d83e2
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2020-04-20 13:16:35 -0700

    add remaining derivatives pseudocode

--- app/services/hyrax/migrator/services/verify_derivatives_service.rb
+++ app/services/hyrax/migrator/services/verify_derivatives_service.rb
@@ -40,6 +40,7 @@ module Hyrax::Migrator::Services
       #     when office_document_mime_types then check_office_document_derivatives(filename)
       #     when audio_mime_types           then check_audio_derivatives(filename)
       #     when video_mime_types           then check_video_derivatives(filename)
+      #     when image_mime_types           then check_image_derivatives(filename)
       #   end
       # end
       #
@@ -62,20 +63,52 @@ module Hyrax::Migrator::Services
     # end
     #
     #
-    # def office_document_mime_types(filename)
+    # def check_office_document_derivatives(filename)
     #
     #   check thumbnail exists?
-    #   check content_ocr exists?
+    #   check extracted_text exists?
     #
     #   check page count (it should match page count from OD1)
     #     page_count = OregonDigital::Derivatives::Image::Utils.page_count(filename)
     #
     #   return true
     # end
-
-
-    # Return derivatives info for the migrated asset (OD2)
+    #
+    # def check_audio_derivatives(filename)
+    #
+    #   check thumbnail exists?
+    #   check mp3 exists?
+    #   check ogg exists?
+    #
+    #   return true
+    # end
+    #
+    # def check_video_derivatives(filename)
+    #
+    #   check thumbnail exists?
+    #   check webm exists?
+    #   check mp4 exists?
+    #
+    #   return true
+    # end
+    #
+    # def check_image_derivatives(filename)
+    #
+    #   check thumbnail (jpg) exists?
+    #   check zoomable (jp2) exists?
+    #
+    #   return true
+    # end
+    #
+    ## Return derivatives info for the migrated asset (OD2)
     # def migrated_info
+    #   return [
+    #     {
+    #       label: :thumbnail,
+    #       format: 'jpg',
+    #       type: 'pdf'
+    #     }
+    #   ]
     # end
   end
 end
