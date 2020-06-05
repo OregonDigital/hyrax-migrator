92ca424a3f7562789a5eaacc973f86bbac957868
lsat12357 <lsato@uoregon.edu>
2020-06-05 07:32:27 -0700

    refactor verify metadata

--- app/services/hyrax/migrator/services/verify_metadata_service.rb
+++ app/services/hyrax/migrator/services/verify_metadata_service.rb
@@ -6,49 +6,69 @@ module Hyrax::Migrator::Services
   ##
   # A service to compare metadata from the source asset and the migrated asset
   class VerifyMetadataService
-    def initialize(migrator_work, migrator_config, profile_dir = nil)
-      @work = migrator_work
-      @profile_dir = profile_dir.nil? ? File.join(@work.working_directory, 'data') : profile_dir
-      @config = migrator_config
+    def initialize(migrator_work, config, hyrax_asset, original_profile)
+      @migrator_work = migrator_work
+      @hyrax_asset = hyrax_asset
+      @original_profile = original_profile
       @new_profile = new_profile
+      @config = config
+      @errors = verify_metadata
     end
 
-    def item
-      @item ||= Hyrax::Migrator::HyraxCore::Asset.find(@work.pid)
-    end
-
-    def content
-      fs = item.file_sets.first
-      fs.original_file.content unless fs.blank?
-    end
-
-    # pull metadata for asset in OD2
     def new_profile
       result_hash = {}
       result_hash[:colls] = colls
       result_hash[:fields] = fields
-      result_hash[:checksums] = checksums
-      result_hash[:admin_set] = item.admin_set_id
+      result_hash[:admin_set] = @hyrax_asset.admin_set_id
       result_hash
+    end
+
+    def map_fields
+      @map_fields ||= YAML.load_file(@config.fields_map)
+    end
+
+    def verify_metadata
+      errors = []
+      @original_profile['fields'].each do |key, val|
+        next if val.blank?
+
+        errors += process_vals(key, val)
+      end
+      errors
+    rescue StandardError => e
+      puts e.message
+    end
+
+    def process_vals(key, val)
+      errors = []
+      val.each do |v|
+        errors << "Unable to verify #{key} in #{@migrator_work.pid}." unless test_val(key, v)
+      end
+      errors
+    end
+
+    def test_val(key, val)
+      new_field = map_fields['fields'][key]
+      return @new_profile[:fields][new_field].include?(val.to_s) if @original_profile['fields'][key].is_a?(Array)
+
+      @new_profile[:fields][new_field] == val.to_s
+    rescue StandardError
+      false
+    end
+
+    def extract(val)
+      str = val.respond_to?(:rdf_subject) ? val.rdf_subject.to_s : val
+      str
     end
 
     def fields
       fields = {}
-      item.as_json.each do |field, val|
+      @hyrax_asset.as_json.each do |field, val|
         next if val.blank?
 
         fields[field] = val.respond_to?(:to_a) ? field_array(val) : extract(val)
       end
       fields
-    end
-
-    def checksums
-      checksums = {}
-      checksums['SHA1hex'] = Digest::SHA1.hexdigest content
-      checksums['SHA1base64'] = Digest::SHA1.base64digest content
-      checksums['MD5hex'] = Digest::MD5.hexdigest content
-      checksums['MD5base64'] = Digest::MD5.base64digest content
-      checksums
     end
 
     def field_array(val)
@@ -61,65 +81,10 @@ module Hyrax::Migrator::Services
 
     def colls
       colls = []
-      item.member_of_collections.each do |coll|
+      @hyrax_asset.member_of_collections.each do |coll|
         colls << coll.id
       end
       colls
-    end
-
-    def verify_metadata
-      errors = []
-      original_profile['fields'].each do |key, val|
-        next if val.blank?
-
-        errors += process_vals(key, val)
-      end
-      errors
-    rescue StandardError => e
-      puts e.message
-    end
-
-    def verify_content
-      errors = []
-
-      original_profile['checksums'].each do |key, val|
-        next if val.blank?
-
-        errors << "Content does not match precomputed #{key} checksums for #{@work.pid}. Source: #{val.first} Migrated: #{@new_profile[:checksums][key]}" unless val.first == @new_profile[:checksums][key]
-      end
-      errors
-    rescue StandardError => e
-      puts e.message
-    end
-
-    def process_vals(key, val)
-      errors = []
-      val.each do |v|
-        errors << "Unable to verify #{key} in #{@work.pid}." unless test_val(key, v)
-      end
-      errors
-    end
-
-    def test_val(key, val)
-      new_field = map_fields['fields'][key]
-      return @new_profile[:fields][new_field].include?(val.to_s) if original_profile['fields'][key].is_a?(Array)
-
-      @new_profile[:fields][new_field] == val.to_s
-    rescue StandardError
-      false
-    end
-
-    def extract(val)
-      str = val.respond_to?(:rdf_subject) ? val.rdf_subject.to_s : val
-      str
-    end
-
-    def original_profile
-      @original_profile ||= YAML.load_file(File.join(@profile_dir, "#{@work.pid}_profile.yml"))
-    end
-
-    def map_fields
-      @map_fields ||= YAML.load_file(@config.fields_map)
     end
   end
 end
--- spec/hyrax/migrator/services/verify_metadata_service_spec.rb
+++ spec/hyrax/migrator/services/verify_metadata_service_spec.rb
@@ -4,20 +4,16 @@ RSpec.describe Hyrax::Migrator::Services::VerifyMetadataService do
   let(:migrator_work) { double }
   let(:hyrax_work) { double }
   let(:config) { Hyrax::Migrator::Configuration.new }
-  let(:service) { described_class.new(migrator_work, config, 'spec/fixtures/data') }
+  let(:original_profile) { YAML.load_file("spec/fixtures/data/#{pid}_profile.yml") }
+  let(:service) { described_class.new(migrator_work, config, hyrax_work, original_profile) }
   let(:pid) { 'df70jh899' }
   let(:set) { double }
   let(:sets) { [set] }
-  let(:file_set) { instance_double('FileSet', id: 'bn999672v', uri: 'http://127.0.0.1/rest/fake/bn/99/96/72/bn999672v') }
-  let(:content_path) { 'spec/fixtures/data/world.png' }
-  let(:original_file) { instance_double('hyrax_original_file', content: File.open(content_path).read) }
 
   before do
     allow(Hyrax::Migrator::HyraxCore::Asset).to receive(:find).and_return(hyrax_work)
     allow(hyrax_work).to receive(:as_json).and_return(json)
     allow(hyrax_work).to receive(:member_of_collections).and_return(sets)
-    allow(hyrax_work).to receive(:file_sets).and_return([file_set])
-    allow(file_set).to receive(:original_file).and_return(original_file)
     allow(migrator_work).to receive(:pid).and_return(pid)
     config.fields_map = 'spec/fixtures/fields_map.yml'
     allow(set).to receive(:id).and_return('joel-palmer')
@@ -51,24 +47,6 @@ RSpec.describe Hyrax::Migrator::Services::VerifyMetadataService do
 
       it 'returns the error' do
         expect(service.verify_metadata).to eq(["Unable to verify identifier in #{pid}."])
-      end
-    end
-  end
-
-  describe 'verify_content' do
-    let(:json) { {} }
-
-    context 'when all checksums values match migrated content' do
-      it 'returns no errors' do
-        expect(service.verify_content).to eq([])
-      end
-    end
-
-    context 'when checksums do not match with migrated content (invalid)' do
-      let(:original_file) { instance_double('hyrax_original_file', content: 'invalid') }
-
-      it 'returns errors' do
-        expect(service.verify_content).to include(match(/Content does not match precomputed/))
       end
     end
   end
