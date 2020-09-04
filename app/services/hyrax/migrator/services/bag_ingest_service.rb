1bee5e6b83516c87db4dc4c4b25a5a7b4a7a9223
lsat12357 <lsato@uoregon.edu>
2020-09-04 15:27:30 -0700

    adding reporter methods

--- app/services/hyrax/migrator/services/bag_ingest_service.rb
+++ app/services/hyrax/migrator/services/bag_ingest_service.rb
@@ -14,23 +14,53 @@ module Hyrax::Migrator::Services
       @input_batch_names = input_batch_names
       @location_service = bag_file_location_service
       @options = options
+      @pids = harvest_pids
     end
 
     def ingest
       # run job for each bag within batch_name
-      location_service.bags_to_ingest.each do |_batch_name, bag_locations|
-        bag_locations.each do |file_path|
-          pid = parse_pid(file_path)
-          if Hyrax::Migrator::HyraxCore::Asset.exists?(pid) && migrator_success?(pid)
-            Rails.logger.warn "Work #{pid} already exists, skipping MigrateWorkJob"
-            next
-          end
-          Hyrax::Migrator::Jobs::MigrateWorkJob.perform_later(args(pid, file_path))
+      @pids.each do |batch_name|
+        @pids[batch_name|.each do |pid|
+        if Hyrax::Migrator::HyraxCore::Asset.exists?(pid) && migrator_success?(pid)
+          Rails.logger.warn "Work #{pid} already exists, skipping MigrateWorkJob"
+          next
         end
+        Hyrax::Migrator::Jobs::MigrateWorkJob.perform_later(args(pid, file_path))
       end
     end
 
+    def batch_report(batch_name, to_file=false)
+      report = {}
+      @pids[batch_name].each do |pid|
+        w = Hyrax::Migrator::Work.find_by_pid(pid)
+        asset = Hyrax::Migrator::HyraxCore::Asset.exists?(pid)
+        report[pid] = "#{w.aasm_state}\t#{w.status}\t#{w.status_message}\t#{asset}" unless w.nil?
+        next
+
+        report[pid] = "migrator work not found"
+      end
+      to_file ? write_to_file(batch_name, report) : write_to_screen(report)
+    end
+
     private
+
+    def write_to_screen(report)
+      puts "Printing aasm_state, status, status_message, and asset.exists? for works in batch #{batch_name}..."
+      report.each do |pid, val|
+        puts "#{pid}: #{val}"
+      end
+    end
+
+    def write_to_file(batch_name, report)
+      puts "Printing aasm_state, status, status_message, and asset.exists? for works in batch #{batch_name}."
+      puts "File will be written to #{migrator_config.file_system_path}"
+      datetime_today = Time.zone.now.strftime('%Y%m%d%H%M%S') # "20171021125903"
+      f = File.open(File.join(migrator_config.file_system_path, "#{batch_name}_report_#{datetime_today}.txt", 'w')
+      report.each do |pid, val|
+        f.puts "#{pid}: #{val}"
+      end
+      f.close
+    end
 
     def args(pid, file_path)
       args = { pid: pid, file_path: file_path }
@@ -48,6 +78,17 @@ module Hyrax::Migrator::Services
 
     def bag_file_location_service
       Hyrax::Migrator::Services::BagFileLocationService.new(input_batch_names, migrator_config)
+    end
+
+    def harvest_pids
+      pid_hash = {}
+      location_service.bags_to_ingest.each do |_batch_name, bag_locations|
+        pid_hash[_batch_name] = []
+        bag_locations.each do |file_path|
+          pid_hash[_batch_name] << parse_pid(file_path)
+        end
+      end
+      pid_hash
     end
   end
 end
