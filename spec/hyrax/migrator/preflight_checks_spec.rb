4279bd9a2042ff02051e6bef4007e6daad14d23e
lsat12357 <lsato@uoregon.edu>
2020-12-15 10:48:48 -0800

    preflight_checks to use new services class

--- spec/hyrax/migrator/preflight_checks_spec.rb
+++ spec/hyrax/migrator/preflight_checks_spec.rb
@@ -14,28 +14,24 @@ class GenericAsset
 end
 
 RSpec.describe Hyrax::Migrator::PreflightChecks do
+  let(:preflight) { described_class.new('spec/fixtures', 'pidlist', true) }
   let(:work) { double }
   let(:nt) { 'spec/fixtures/3t945r08v/data/3t945r08v_descMetadata.nt' }
-  let(:datastreams) { double }
-  let(:datastream) { double }
   let(:graph) { RDF::Graph.load(nt) }
   let(:workflow) { double }
-  let(:service) { described_class.new(path, 'pidlist', true) }
-  let(:path) { 'spec/fixtures' }
+  let(:services) { {} }
   let(:read_groups) { ['public'] }
   let(:descmeta) { double }
 
   before do
     allow(GenericAsset).to receive(:find).and_return(work)
-    allow(work).to receive(:datastreams).and_return(datastreams)
-    allow(datastreams).to receive(:[]).with('descMetadata').and_return(datastream)
     allow(work).to receive(:workflowMetadata).and_return(workflow)
     allow(workflow).to receive(:reviewed).and_return(true)
     allow(workflow).to receive(:destroyed).and_return(false)
-    allow(datastream).to receive(:graph).and_return(graph)
     allow(work).to receive(:read_groups).and_return(read_groups)
     allow(work).to receive(:descMetadata).and_return(descmeta)
     allow(descmeta).to receive(:accessRestrictions).and_return([])
+    allow(descmeta).to receive(:graph).and_return(graph)
     allow(work).to receive(:pid).and_return('3t945r08v')
   end
 
@@ -47,16 +43,16 @@ RSpec.describe Hyrax::Migrator::PreflightChecks do
   end
 
   describe 'verify' do
-    context 'when there are errors' do
-      it 'writes the pid under review to errors' do
-        service.verify
-        expect(service.instance_variable_get(:@errors)).to include 'Working on abcde1234...'
+    context 'when processing a given pid' do
+      it 'writes the pid to errors' do
+        preflight.verify
+        expect(preflight.instance_variable_get(:@errors)).to include 'Working on oregondigital:abcde5678...'
       end
 
       context 'when there is an error' do
         it 'writes it to errors' do
-          service.verify
-          errors = service.instance_variable_get(:@errors)
+          preflight.verify
+          errors = preflight.instance_variable_get(:@errors)
           err_message = errors.any? { |e| e.start_with? 'Predicate not found' }
           expect(err_message).to equal true
         end
@@ -70,8 +66,8 @@ RSpec.describe Hyrax::Migrator::PreflightChecks do
         end
 
         it 'handles the error' do
-          service.verify
-          expect(service.instance_variable_get(:@errors)).to include('System error: Not found')
+          preflight.verify
+          expect(preflight.instance_variable_get(:@errors)).to include('System error: Not found')
         end
       end
     end
@@ -84,19 +80,36 @@ RSpec.describe Hyrax::Migrator::PreflightChecks do
       end
 
       it 'adds the error' do
-        service.verify
-        expect(service.instance_variable_get(:@errors)).to include 'status: destroyed'
+        preflight.verify
+        expect(preflight.instance_variable_get(:@errors)).to include 'status: destroyed'
+      end
+    end
+  end
+
+  describe 'bump_counters' do
+    context 'when there is a cpd' do
+      let(:cpd_service) { double }
+
+      before do
+        allow(Hyrax::Migrator::CpdCheck).to receive(:new).and_return(cpd_service)
+        allow(cpd_service).to receive(:work=)
+        allow(cpd_service).to receive(:check_cpd).and_return('cpd')
+      end
+
+      it 'bumps the counter' do
+        preflight.verify
+        expect(preflight.instance_variable_get(:@counters)[:cpds]).to eq 2
       end
     end
   end
 
   describe 'verbose_display' do
-    context 'when there are valid attributes and verbose is true' do
+    context 'when there are errors and verbose is true' do
       it 'displays them' do
         printed = capture_stdout do
-          service.verify
+          preflight.verify
         end
-        expect(printed).to include('title')
+        expect(printed).to include('Predicate not found')
       end
     end
   end
