e358423a3bd9c26742c3ebb34119007fe9e93e83
lsat12357 <lsato@uoregon.edu>
2021-01-11 07:21:16 -0800

    use hyrax's checksum

--- app/services/hyrax/migrator/services/verify_checksums_service.rb
+++ app/services/hyrax/migrator/services/verify_checksums_service.rb
@@ -6,31 +6,18 @@ module Hyrax::Migrator::Services
   ##
   # A service to compare the checksums from the source asset and the migrated asset
   class VerifyChecksumsService
+    ALGORITHM = 'MD5hex'
+
     def initialize(hyrax_asset, profile_dir)
       @hyrax_asset = hyrax_asset
       @profile_dir = profile_dir
-      @new_profile = new_profile
-      @errors = []
     end
 
-    def content
-      fs = @hyrax_asset.file_sets.first
-      @content = fs.blank? ? '' : fs.original_file.content
-    end
-
-    def new_profile
-      result_hash = {}
-      result_hash[:checksums] = checksums
-      result_hash
-    end
-
-    def checksums
-      checksums = {}
-      checksums['SHA1hex'] = Digest::SHA1.hexdigest content
-      checksums['SHA1base64'] = Digest::SHA1.base64digest content
-      checksums['MD5hex'] = Digest::MD5.hexdigest content
-      checksums['MD5base64'] = Digest::MD5.base64digest content
-      checksums
+    ## Hyrax already calculates checksum using MD5hex
+    def new_content_checksum
+      @hyrax_asset.file_sets.first.original_file.original_checksum.first
+    rescue StandardError
+      ''
     end
 
     def original_checksums
@@ -42,12 +29,9 @@ module Hyrax::Migrator::Services
     def verify_content
       return [] if original_checksums['checksums'].blank?
 
-      original_checksums['checksums'].each do |key, val|
-        next if val.blank?
+      return ["Content does not match precomputed #{ALGORITHM} checksums for #{@hyrax_asset.id}."] unless original_checksums['checksums'][ALGORITHM].first == new_content_checksum
 
-        @errors << "Content does not match precomputed #{key} checksums for #{@hyrax_asset.id}." unless val.first == @new_profile[:checksums][key]
-      end
-      @errors
+      []
     end
   end
 end
--- spec/hyrax/migrator/services/verify_checksums_service_spec.rb
+++ spec/hyrax/migrator/services/verify_checksums_service_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe Hyrax::Migrator::Services::VerifyChecksumsService do
   let(:pid) { 'df70jh899' }
   let(:file_set) { instance_double('FileSet', id: 'bn999672v', uri: 'http://127.0.0.1/rest/fake/bn/99/96/72/bn999672v') }
   let(:content_path) { 'spec/fixtures/data/world.png' }
-  let(:original_file) { instance_double('hyrax_original_file', content: File.open(content_path).read) }
+  let(:original_file) { double }
+  let(:original_checksum) { ['28da6259ae5707c68708192a40b3e85c'] }
 
   before do
     allow(Hyrax::Migrator::HyraxCore::Asset).to receive(:find).and_return(hyrax_work)
     allow(hyrax_work).to receive(:as_json).and_return(json)
     allow(hyrax_work).to receive(:file_sets).and_return([file_set])
     allow(file_set).to receive(:original_file).and_return(original_file)
+    allow(original_file).to receive(:original_checksum).and_return(original_checksum)
     allow(hyrax_work).to receive(:id).and_return(pid)
   end
 
@@ -27,9 +29,19 @@ RSpec.describe Hyrax::Migrator::Services::VerifyChecksumsService do
     end
 
     context 'when checksums do not match with migrated content (invalid)' do
-      let(:original_file) { instance_double('hyrax_original_file', content: 'invalid') }
+      let(:original_checksum) { ['invalid'] }
 
       it 'returns errors' do
+        expect(service.verify_content).to include(match(/Content does not match precomputed/))
+      end
+    end
+
+    context 'when there is a checksum profile but no hyrax file' do
+      before do
+        allow(original_checksum).to receive(:first).and_raise StandardError
+      end
+
+      it 'reports the mismatch' do
         expect(service.verify_content).to include(match(/Content does not match precomputed/))
       end
     end
