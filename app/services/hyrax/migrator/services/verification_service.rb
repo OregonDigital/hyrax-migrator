2bb98aef9c7f4b9cffb3388c00ac342a1d69651e
lsat12357 <lsato@uoregon.edu>
2021-01-27 11:06:49 -0800

    remove working_directory after verification

--- app/services/hyrax/migrator/services/verification_service.rb
+++ app/services/hyrax/migrator/services/verification_service.rb
@@ -30,6 +30,7 @@ module Hyrax::Migrator::Services
       errors << @checksums_service.verify_content
       errors << @derivatives_service.verify
       errors << @children_service.verify_children
+      @work.remove_temp_directory
       errors
     rescue StandardError => e
       errors << "Encountered an error while working on #{@work.pid}: #{e.message}"
