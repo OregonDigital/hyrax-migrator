be335144f32d7986e1550bf987af6464893abc4a
lsat12357 <lsato@uoregon.edu>
2021-02-02 13:45:24 -0800

    refactor visibility lookup

---
+++ app/services/hyrax/migrator/services/verify_visibility_service.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal:true
+
+module Hyrax::Migrator::Services
+  # A service to verify that the asset's visibility is correctly assigned
+  class VerifyVisibilityService < VerifyService
+    include Hyrax::Migrator::VisibilityMethods
+    def verify
+      return 'visibility error' if lookup(@migrated_work.original_profile['visibility'])[:visibility] != @migrated_work.asset.visibility
+
+      ''
+    end
+  end
+end
