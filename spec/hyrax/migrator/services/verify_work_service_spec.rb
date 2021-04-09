2c8bbe5b1835ea2496c885619fff46c44bcdadd4
lsat12357 <lsato@uoregon.edu>
2021-04-09 07:10:46 -0700

    enable list of verifiers

--- spec/hyrax/migrator/services/verify_work_service_spec.rb
+++ spec/hyrax/migrator/services/verify_work_service_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Hyrax::Migrator::Services::VerifyWorkService do
-  let(:service) { described_class.new(pid: pid) }
+  let(:service) { described_class.new(args) }
+  let(:args) { { pid: pid } }
   let(:verification_service) { double }
   let(:work) { create(:work, pid: pid, env: {}) }
   let(:pid) { 'abcde1234' }
@@ -30,6 +31,26 @@ RSpec.describe Hyrax::Migrator::Services::VerifyWorkService do
     it 'returns without editing the work' do
       service.run
       expect(work).not_to receive(:save)
+    end
+  end
+
+  describe 'running the service with a custom list of services' do
+    let(:args) { { pid: pid, verify_services: ['Hyrax::Migrator::Services::VerifyMetadataService'] } }
+    let(:verify_metadata_service) { double }
+    let(:migrated_work) { double }
+
+    before do
+      allow(Hyrax::Migrator::Services::VerificationService::MigratedWork).to receive(:new).and_return(migrated_work)
+      allow(Hyrax::Migrator::Services::VerifyMetadataService).to receive(:new).and_return(verify_metadata_service)
+      allow(verify_metadata_service).to receive(:verify).and_return([])
+      allow(verification_service).to receive(:verify).and_return([])
+      allow(migrated_work).to receive(:work).and_return(work)
+    end
+
+    it 'only instantiates VerifyMetadataService' do
+      expect(Hyrax::Migrator::Services::VerifyChecksumsService).not_to receive :new
+      expect(Hyrax::Migrator::Services::VerifyMetadataService).to receive :new
+      service.run
     end
   end
 end
